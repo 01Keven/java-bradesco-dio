0284ad2fe675e4fad8361d304069c93b95cb3e27
01Keven <kevenchristiancode@gmail.com>
2025-03-07 18:12:20 -0300

    feat: concatenar strings e anatomia de classes e metodos

---
+++ src/ConcatenarString.java
@@ -0,0 +1,13 @@
+public class ConcatenarString {
+    public static void main(String[] args) {
+        String primeiroNome = "keven";
+        String segundoNome = "christian";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
