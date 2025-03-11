77a8e1255be1d0720078fbbf0091057bdae66025
01Keven <kevenchristiancode@gmail.com>
2025-03-11 20:48:02 -0300

    atividade: função simples para calcular a media de boletim de um aluno

---
+++ src/BoletimEscolar.java
@@ -0,0 +1,15 @@
+public class BoletimEscolar {
+    public static void main(String[] args) {
+        double media = 7.0;
+        double notaAluno = notaMedia(7.0, 7.0);
+        if (notaAluno < media) {
+            System.out.println("Aluno Reprovou!");
+        } else {
+            System.out.println("Aluno passou! nota: " + notaAluno);
+        }
+    }
+
+    public static double notaMedia(double primeiraNota, double segundaNota) {
+        return (primeiraNota + segundaNota) / 2;
+    }
+}
